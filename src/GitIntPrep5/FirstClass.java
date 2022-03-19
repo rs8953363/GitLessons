ff0d30001a0f981e9bf45180f9f10749b85e656b
Rose <rs8953363@gmail.com>
2022-03-19 17:03:19 -0500

    v003

--- src/GitIntPrep5/FirstClass.java
+++ src/GitIntPrep5/FirstClass.java
@@ -1,5 +1,17 @@
 package GitIntPrep5;
 
 public class FirstClass {
-
+	public static void main(String[] args) {
+		//Git: version control
+		//Github: Remote repository
+		//git init: creates an empty repository(folder) named git in your project. It's used only once.
+		//git add : To add the changes to staging area
+		//git commit -m"v001": To add the changes in staging area to local repository. Commit=version
+		//Staging area helps us to go back. Do you want to create a version?
+		//git log: commit list, change history
+		//git status: status 
+		//1st line
+		//2nd line
+		//3rd line 
+	}
 }
