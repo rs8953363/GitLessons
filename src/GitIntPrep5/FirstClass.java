ec4248b1d0020edea6868a7414618c77171819f9
Rose <rs8953363@gmail.com>
2022-03-19 23:09:39 -0500

    v007

--- src/GitIntPrep5/FirstClass.java
+++ src/GitIntPrep5/FirstClass.java
@@ -4,12 +4,14 @@ public class FirstClass {
 	public static void main(String[] args) {
 		//Git: version control
 		//Github: Remote repository
-		//git init: creates an empty repository(folder) named git in your project. It's used only once.
-		//git add : To add the changes to staging area
-		//git commit -m"v001": To add the changes in staging area to local repository. Commit=version
-		//Staging area helps us to go back. Do you want to create a version?
-		//git log: commit list, change history
-		//git status: status 
+		
+		/*git init: creates an empty repository(folder) named git in your project. It's used only once.
+		  git add : To add the changes to staging area
+		  git commit -m"v001": To add the changes in staging area to local repository. Commit=version
+		  Staging area helps us to go back. Do you want to create a version?
+		  git log: commit list, change history
+		  git status: check status 
+		  */
 		//1st line
 		//2nd line
 		
@@ -17,5 +19,16 @@ public class FirstClass {
 		//5th line
 		//6th line
 		
+		/*To push your codes from your local repository to remote one
+		 * git add .
+		 * git commit -m"mesaj"
+		 * git push
+		 */
+		
+		/* To import the updated codes to your repository and see changes
+		 * git fetch (gets the changes)
+		 * git merge (compares the changes)
+		 * git pull
+		 */
 	}
 }
