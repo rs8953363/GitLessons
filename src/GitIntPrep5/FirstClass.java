d62793252d26e5463eb6a2464ee0f764fac8ae54
Rose <rs8953363@gmail.com>
2022-03-20 06:29:41 -0500

    v005

--- src/GitIntPrep5/FirstClass.java
+++ src/GitIntPrep5/FirstClass.java
@@ -45,15 +45,16 @@ public class FirstClass {
 		 * git commit -m"v001 Gul" (to save your changes in your local branch)
 		 * git branch (to see which branch you are at)
 		 * git checkout main (to switch the main branch. You merge your files while you are in main branch)
-		 * git merge Gul (You type this command while you're in main branch
 		 * git add .(type it while you are in main branch)
 		 * git commit -m"v002"(type it while you are in main branch) 
+		 * git merge Gul (You type this command while you're in main branch
 		 * git push (type it while you are in main branch) 
 		 * git checkout Gul (to go back to your branch)
 		 * If you want to push the current branch and set the remote as upstream, use
    			git push --set-upstream origin Gul (OR git push -u origin Gul)(This is required only for the first time. If you do more changes on 
    			your file and want to push your code you can just use git push)
 		 * Don't forget to save changes each time you update your code
+		 * Branch name: feature (Instead of your name, usually "feature is used in work environment)
 		 */
 		
 		//11th line
